1efe25aad7d720d6f3524bb934fda4feb60bc46d
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-01 16:20:43 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO series (title, subgenre_id, author_id) VALUES ("for whom the bell tolls", 1, 1), ("a  moveable feast")
+INSERT INTO series (title, subgenre_id, author_id) VALUES ("for whom the bell tolls", 1, 1), ("a  moveable feast", 2, 1)
