806275a7db99f567db98782eee8d1a0541593017
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-01 16:09:58 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,6 +1,11 @@
-CREATE TABLE series(
+CREATE TABLE series (
   id INTEGER PRIMARY KEY,
   title TEXT,
   subgenre_id INTEGER,
   author_id INTEGER
+);
+
+CREATE TABLE subgenres (
+  id INTEGER PRIMARY KEY,
+  name TEXT
 );
