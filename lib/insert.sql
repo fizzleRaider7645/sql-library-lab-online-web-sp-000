58d50d5c54a614f625d98078d4922b1468b6003e
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-01 16:24:57 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,3 @@
 INSERT INTO series (title, subgenre_id, author_id) VALUES ("for whom the bell tolls", 1, 1), ("a  moveable feast", 2, 1);
 INSERT INTO books (title, year, series_id) VALUES ('One Hundred Years of Solitude', 1992, 1), ("Tender is the Night", 1992, 2), ("Wuthering Heights", 1992, 3), ("Pride and Prejudice", 1992, 4), ("A Brief History Of Time", 1992, 5), ("Our Mutual Friend", 1992, 6);
+INSERT INTO characters ()
